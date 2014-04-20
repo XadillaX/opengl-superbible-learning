b9a5bfb9ec4a7e869949230a29847bfe64f07579
XadillaX <admin@xcoder.in>
2014-04-20 21:40:45 +0800

    特殊键移动画面

--- Triangle/triangle.cpp
+++ Triangle/triangle.cpp
@@ -2,8 +2,16 @@
 #include <GLShaderManager.h>
 #include <GL/glut.h>
 
-GLBatch triangleBatch;
+GLBatch squareBatch;
 GLShaderManager shaderManager;
+
+GLfloat blockSize = 0.1f;
+GLfloat vVerts[] = {
+    -blockSize, -blockSize, 0.0f, 
+	blockSize, -blockSize, 0.0f,
+	blockSize,  blockSize, 0.0f,
+	-blockSize,  blockSize, 0.0f
+};
 
 // 改变窗体大小函数
 void ChangeSize(int w, int h)
@@ -18,7 +26,7 @@ void RenderScene()
 
     GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
     shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
-    triangleBatch.Draw();
+    squareBatch.Draw();
 
     glutSwapBuffers();
 }
@@ -31,16 +39,49 @@ void SetupRC()
     // 初始化着色器实例
     shaderManager.InitializeStockShaders();
 
-    // 载入一个三角形
-    GLfloat vVerts[] = {
-        -0.5f, 0.0f, 0.0f,
-        0.5f, 0.0f, 0.0f,
-        0.0f, 0.5f, 0.0f
-    };
+    // 建立四边形批次
+    squareBatch.Begin(GL_TRIANGLE_FAN, 4);
+    squareBatch.CopyVertexData3f(vVerts);
+    squareBatch.End();
+}
 
-    triangleBatch.Begin(GL_TRIANGLES, 3);
-    triangleBatch.CopyVertexData3f(vVerts);
-    triangleBatch.End();
+void SpecialKeys(int key, int x, int y)
+{
+    // 步长
+    GLfloat stepSize = 0.025f;
+
+    GLfloat blockX = vVerts[0];     ///< 左下X
+    GLfloat blockY = vVerts[7];     ///< 右上Y
+
+    if(key == GLUT_KEY_UP)  blockY += stepSize;
+    if(key == GLUT_KEY_DOWN) blockY -= stepSize;
+    if(key == GLUT_KEY_LEFT) blockX -= stepSize;
+    if(key == GLUT_KEY_RIGHT) blockX += stepSize;
+
+    // 碰撞检测
+    if(blockX < -1.0f) blockX = -1.0f;
+    if(blockX > (1.0f - blockSize * 2)) blockX = 1.0f - blockSize * 2;
+    if(blockY < -1.0f + blockSize * 2) blockY = -1.0f + blockSize * 2;
+    if(blockY > 1.0f) blockY = 1.0f;
+
+    // 第一个点
+    vVerts[0] = blockX;
+    vVerts[1] = blockY - blockSize * 2;
+
+    // 第二个点
+    vVerts[3] = blockX + blockSize * 2;
+    vVerts[4] = blockY - blockSize * 2;
+
+    // 第三个点
+    vVerts[6] = blockX + blockSize * 2;
+    vVerts[7] = blockY;
+
+    // 第四个点
+    vVerts[9] = blockX;
+    vVerts[10] = blockY;
+
+    squareBatch.CopyVertexData3f(vVerts);
+    glutPostRedisplay();
 }
 
 int main(int argc, char* argv[])
@@ -49,9 +90,7 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Triangle");
-    glutReshapeFunc(ChangeSize);
-    glutDisplayFunc(RenderScene);
+    glutCreateWindow("Move Block with Arrow Keys");
 
     GLenum err = glewInit();
     if(GLEW_OK != err)
@@ -59,6 +98,10 @@ int main(int argc, char* argv[])
         fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
         return 1;
     }
+
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutSpecialFunc(SpecialKeys);
 
     SetupRC();
     glutMainLoop();
